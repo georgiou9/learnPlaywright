161f5801de9bb30731f18594ae63b9ef4e8a1c00
George G <georgiou.gea@gmail.com>
2023-11-22 03:02:51 -0600

    Fixes broken locator for assertion

--- learnPlaywright/Hooks/Hooks.cs
+++ learnPlaywright/Hooks/Hooks.cs
@@ -8,26 +8,35 @@ namespace learnPlaywright.Hooks
     {
         // this property will be called in the tests
         // this property will give us more control over the browser context
-        public IPage Page { get; private set; } = null!;
+        public IPage Page { get; private set; } = null;
+        public IBrowserContext Context1 { get; private set; }
 
         // will be excecuting these steps before each schenario
         [BeforeScenario]
         public async Task RegisterSingleInstancePractitioner()
         {
-            //initialise Playwrigth
+            // initialise Playwrigth
             var playwright = await Playwright.CreateAsync();
-            //initialise Browser
+
+            // initialise Browser
             var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
             {
                 Headless = false,
                 Timeout = 3_000
             });
 
-            //setup browser context
-            var context1 = await browser.NewContextAsync();
+            // setup browser context
+            Context1 = await browser.NewContextAsync();
 
-            //initialise a page on the browser context
-            Page = await context1.NewPageAsync();
+            // initialise a page on the browser context
+            Page = await Context1.NewPageAsync();
+        }
+
+        [AfterScenario]
+        public async Task TearDownBrowserAsync()
+        {
+            await Context1.CloseAsync();
+            await Page.CloseAsync();
         }
     }
 }
